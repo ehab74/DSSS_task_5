296bf7b9ed1698e9efbbbf401a05178eba92f20a
Ehab Wahba <ehabwahba4@gmail.com>
2022-12-04 19:36:29 +0100

    fix a bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = "snowflake",
     version = "0.1",
     author = "Ehab",
-    author_email = "ehab.wahba@fau.de"
+    author_email = "ehab.wahba@fau.de",
     packages = find_packages(),
     install_requires = ["numpy","turtle"],
     )

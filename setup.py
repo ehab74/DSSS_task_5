31a1251973735fe7a4dc3ce8b23346f331e94433
Ehab Wahba <ehabwahba4@gmail.com>
2022-12-04 19:34:01 +0100

    rename package

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages
 
 setup(
     name = "snowflake",
-    version="0.1",
-    author="Ehab",
-    author_email="ehab.wahba@fau.de"
-    packages=find_packages(),
+    version = "0.1",
+    author = "Ehab",
+    author_email = "ehab.wahba@fau.de"
+    packages = find_packages(),
     install_requires = ["numpy","turtle"],
-)
+    )
